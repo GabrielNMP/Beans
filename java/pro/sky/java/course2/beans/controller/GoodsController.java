57a3ebb7afb8fa0ef3c5992eec21995b247c1d08
Gabriel_n3 <p705865@gmail.com>
2022-07-06 20:56:29 +0300

    ready HW

---
+++ java/pro/sky/java/course2/beans/controller/GoodsController.java
@@ -0,0 +1,33 @@
+package pro.sky.java.course2.beans.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import pro.sky.java.course2.beans.service.GoodsService;
+
+import java.util.Collections;
+import java.util.List;
+
+
+@RestController
+@RequestMapping("/order")
+public class GoodsController {
+
+    private final GoodsService goodsService;
+
+    public GoodsController(GoodsService goodsService) {
+        this.goodsService = goodsService;
+    }
+    @GetMapping("/add")
+    public List<List<Integer>> addGoods (@RequestParam Integer id) {
+        return goodsService.addGoods(Collections.singletonList(id));
+    }
+    @GetMapping("/get")
+    public List<Integer> getGoods(){
+        return goodsService.getGoods();
+    }
+
+
+
+}
---
+++ java/pro/sky/java/course2/beans/service/GoodsServiceImpl.java
@@ -0,0 +1,36 @@
+package pro.sky.java.course2.beans.service;
+
+
+import org.springframework.stereotype.Service;
+import pro.sky.java.course2.beans.basket.Goods;
+
+import java.util.List;
+
+@Service
+public class GoodsServiceImpl implements GoodsService {
+
+    private final Goods goods;
+
+    public GoodsServiceImpl(Goods goods){
+        this.goods = goods;
+    }
+    // private final List<Goods> goodsList;
+
+    //public GoodsServiceImpl(Goods goods) {
+    //    this.goods = goods;
+    //    this.goodsList = new ArrayList<>();
+    //}
+
+    @Override
+    public List<List<Integer>> addGoods(List<Integer> id) {
+        return goods.addGood(id);
+    }
+
+    @Override
+    public List<Integer> getGoods(){
+        return goods.getGoods();
+    }
+
+
+
+}
