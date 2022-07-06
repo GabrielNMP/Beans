a559a30445a7f3fd1ee8c73de133dc7436905cdc
Gabriel_n3 <p705865@gmail.com>
2022-07-06 14:27:06 +0300

    Initial commit

---
+++ java/pro/sky/java/course2/beans/BeansApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.java.course2.beans;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeansApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BeansApplication.class, args);
+    }
+
+}
